a7832ad1f7d1cbdc753f70eb69dbd45895e61cd0
Denzel Hooke <denzelhooke@hotmail.com>
2023-07-07 22:52:23 -0700

    stuff

--- src/app/components/UserInformItem.js
+++ src/app/components/UserInformItem.js
@@ -3,8 +3,12 @@ import styles from '../styles/userInform.module.scss';
 const UserInformItem = ({ icon, title, body }) => {
   return (
     <div className={`${styles.item} `}>
-      <div className={styles.icon__wrapper}>{icon}</div>
-      <h4 className="medium-sub-heading">{title}</h4>
+      <div className={styles.item__heading__wrapper}>
+        <div>
+          <div className={styles.icon__wrapper}>{icon}</div>
+          <h4 className="medium-sub-heading">{title}</h4>
+        </div>
+      </div>
       <p className="default-text">{body}</p>
     </div>
   );
